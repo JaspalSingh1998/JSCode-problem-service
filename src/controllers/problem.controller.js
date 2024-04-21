c5ace67245fc2af44c23b1dea88ee8aed75d98b6
JaspalSingh1998 <jaspalsinghpersonal9@gmail.com>
2024-04-21 12:53:04 -0400

    Feature: Completed api to create new problem

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,12 +1,18 @@
 const {StatusCodes} = require('http-status-codes');
 const NotImplemented = require('../errors/notImplemented.error');
+const {ProblemService} = require('../services')
+const {ProblemRepository} = require('../repositories');
+
+const problemService = new ProblemService( new ProblemRepository());
+
 function pingProblemController(req, res) {
     return res.json({message: 'Ping Controller is up'})
 }
 
-function addProblem(req, res, next) {
+async function addProblem(req, res, next) {
   try {
-    throw new NotImplemented('addProblem');
+   const newProblem = await problemService.createProblem(req.body);
+   return res.status(StatusCodes.CREATED).json({success: true, message: 'Successfully created a new problem.', error: {}, data: newProblem})
   } catch (error) {
     next(error)
   }
---
+++ src/services/problem.service.js
@@ -0,0 +1,23 @@
+const sanitizeMarkdown = require("../utils/markdownSanitizer");
+
+class ProblemService {
+    constructor(problemRepository) {
+        this.problemRepository = problemRepository;
+    }
+
+    async createProblem(problemData) {
+       try {
+         // 1. Sanitize markdown for description
+         problemData.description = sanitizeMarkdown(problemData.description)
+
+         const problem = await this.problemRepository.createProblem(problemData);
+ 
+         return problem;
+       } catch (error) {
+            console.log(error);
+            throw error;
+       }
+    }
+}
+
+module.exports = ProblemService;
